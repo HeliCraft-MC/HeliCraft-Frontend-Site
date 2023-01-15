b2549f116ad981fb3181de0ea4cff1702988b1ad
ms0ur <maksimsuslov7142@gmail.com>
2023-01-15 12:31:09 +0300

    I forget to edit this small thing

--- api/navbar.php
+++ api/navbar.php
@@ -4,14 +4,14 @@
         
         <nav class="navbar navbar-expand-lg fs-5">
             <div class="container-fluid">
-              <a class="navbar-brand text-white font2" href="#">HeliCraft</a>
+              <a class="navbar-brand text-white font2" href="/">HeliCraft</a>
               <button class="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon text-white"></span>
               </button>
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                   <li class="nav-item">
-                    <a class="nav-link active text-white" aria-current="page" href="#"><i class="bi bi-bookmark"></i> Главная</a>
+                    <a class="nav-link text-white" aria-current="page" href="/"><i class="bi bi-bookmark"></i> Главная</a>
                   </li>
                   <li class="nav-item">
                     <a class="nav-link text-white" href="/rules"><i class="bi bi-book"></i> Правила</a>
