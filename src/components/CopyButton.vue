e56af22fb74499337b0b5266a67b70235745bdfa
ms0ur <maksimsuslov7142@gmail.com>
2023-07-17 15:50:55 +0300

    Added button "Скопировать IP сервера"(Copy server IP)

--- src/components/CopyButton.vue
+++ src/components/CopyButton.vue
@@ -1,5 +1,38 @@
-<script></script>
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import { useClipboard } from '@vueuse/core';
+
+export default defineComponent({
+  name: 'CopyButton',
+  props: {
+    cp: {
+      type: String,
+      required: true
+    }
+  },
+  setup(props) {
+    const copyToClipboard = () => {
+      const { copy } = useClipboard();
+      copy(props.cp);
+      buttonText.value = '<i class="bi bi-clipboard-check-fill"></i>Скопировано';
+      setTimeout(() => {
+        buttonText.value = '<i class="bi bi-clipboard-fill"></i> Скопировать IP сервера';
+      }, 2000);
+    };
+
+    const buttonText = ref('<i class="bi bi-clipboard-fill"></i> Скопировать IP сервера');
+
+    return {
+      buttonText,
+      copyToClipboard
+    };
+  }
+});
+</script>
+
 <template>
-    <button></button>
+    <button :cp="'Скопировать IP сервера'" v-html="buttonText" @click="copyToClipboard()" class="rounded-lg text-white bg-purple-700 py-2 px-4"></button>
 </template>
-<style scoped></style>
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,16 @@
-<script lang="ts"></script>
+<script lang="ts">
+import CopyButtonN from '@/components/CopyButton.vue';
+export default {
+    components: {
+        CopyButtonN
+    }
+}
+</script>
 <template>
   <header class="flex flex-col hscreenh justify-center items-center text-white">
     <h1 class="headers text-5xl">HeliCraft</h1>
     <h2 class="text-lg">Minecraft сервер с королевствами</h2>
+    <CopyButtonN cp="mc.helicraft.ru" class="mt-5" />
   </header>
   <main class="flex bg-violet-800 w-full h-96 text-white justify-center items-center text-lg">
     <p>
