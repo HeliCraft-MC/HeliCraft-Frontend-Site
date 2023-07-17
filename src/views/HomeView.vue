62ba5861f3e7f4cf96f790b70a5bae43b07a074c
ms0ur <maksimsuslov7142@gmail.com>
2023-07-17 12:23:59 +0300

    Main page ready

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script lang="ts"></script>
+<template>
+    <header class="flex flex-col hscreenh justify-center items-center text-white">
+        <h1 class="headers text-5xl">HeliCraft</h1>
+        <h2 class="text-lg">Minecraft сервер с королевствами</h2>
+    </header>
+    <main class="flex bg-violet-800 w-full h-96 text-white justify-center items-center text-lg">
+        <p>Наш сервер был основан в августе 2020 года, и с того момента мы продолжаем развивать его. Посмотрите другие разделы сайта, чтобы узнать подробнее о правилах и проч.</p>
+    </main>
+</template>
+<style scoped>
+    .hscreenh{
+        height: 95vh;
+    }
+</style>
